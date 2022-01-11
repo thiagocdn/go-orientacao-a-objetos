3a272811d512aa4d5ed80507ed29a78b8707c9d8
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-01-10 22:24:48 -0300

    Primeira aula, variaveis

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	var titular string = "Thiago"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 258563.53
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
 }
